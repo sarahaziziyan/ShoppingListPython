c9e3cb386d904f92f003820ef9e49ec67d307f00
Sarah Aziziyan <aziziyan.sarah@gmail.com>
2019-11-27 15:33:42 +0330

    change the list to a dictionary and add function add_item

--- shoppingList.py
+++ shoppingList.py
@@ -1,4 +1,4 @@
-shopping_list = list()
+shopping_list = dict()
 
 def main():
     while(True):
@@ -7,18 +7,32 @@ def main():
             print_help()
         elif whatToDo=="ca":
             add_category()
+        elif whatToDo=="item":
+            add_item()
+        elif whatToDo=="print":
+            print_shopping_list()
         elif whatToDo=="exit":
             break
 
 def print_help():
     print("To add a category-> ca")
-    print("To add number of items to a category-><category> <numberOfItems>  ex:fruit 4")
+    print("To add number of items to a category->item")
     print("To print the shopping list-> print")
     print("To exit-> exit")
 
 def add_category():
     category = input("please type the name of the category: ")
-    shopping_list.append({category:0})
+    shopping_list[category] = 0
     print(shopping_list)
+
+def print_shopping_list():
+    print(shopping_list)
+
+def add_item():
+    category = input("enter category: ")
+    number_items = input("number of items: ")
+    shopping_list[category] = number_items
+
+
 
 main()
