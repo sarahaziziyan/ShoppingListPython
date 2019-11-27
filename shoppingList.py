3b828d0ba4830c616ab70114ab153985b5f50ba9
Sarah Aziziyan <aziziyan.sarah@gmail.com>
2019-11-27 15:19:15 +0330

    exit functions and add category function added

--- shoppingList.py
+++ shoppingList.py
@@ -1,13 +1,24 @@
 shopping_list = list()
 
 def main():
-    whatToDo = input("type help in any stage to get help/type the instruction:")
-    if whatToDo=="help":
-        printHelp()
+    while(True):
+        whatToDo = input("help/instruction: ")
+        if whatToDo=="help":
+            print_help()
+        elif whatToDo=="ca":
+            add_category()
+        elif whatToDo=="exit":
+            break
 
-def printHelp():
+def print_help():
     print("To add a category-> ca")
     print("To add number of items to a category-><category> <numberOfItems>  ex:fruit 4")
     print("To print the shopping list-> print")
+    print("To exit-> exit")
+
+def add_category():
+    category = input("please type the name of the category: ")
+    shopping_list.append({category:0})
+    print(shopping_list)
 
 main()
